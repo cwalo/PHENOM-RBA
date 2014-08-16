2b129606cf2b7177943bf38f70480641b2f322fd
Corey Walo <cjwalo@gmail.com>
2014-08-16 14:21:02 -0500

    Initial Commit

---
+++ PhenomRBA/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PhenomRBA
+//
+//  Created by Corey Walo on 8/16/14.
+//  Copyright (c) 2014 Audio Armada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PhenomRBA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PhenomRBA
+//
+//  Created by Corey Walo on 8/16/14.
+//  Copyright (c) 2014 Audio Armada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
