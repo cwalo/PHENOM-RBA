63896e35bdd4dc7a7301e2dbd914dd732b9e5471
Corey Walo <cjwalo@gmail.com>
2015-10-22 00:47:01 -0500

    add AAAudioFileSampler class

---
+++ PhenomRBA/AAAudioFileSampler.h
@@ -0,0 +1,38 @@
+//
+//  AAAudioFileSampler.h
+//  AACore
+//
+//  Created by Corey Walo on 4/26/15.
+//  Copyright (c) 2015 Corey Walo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import <CoreAudio/CoreAudioTypes.h>
+
+typedef struct AUGraphPlayer
+{
+    AudioStreamBasicDescription inputFormat; // input file's data stream description
+    AudioFileID					inputFile; // reference to your input file
+    
+    AUGraph graph;
+    AudioUnit fileAU;
+    
+} AUGraphPlayer;
+
+@interface AAAudioFileSampler : NSObject
+{
+    NSArray *audioResourceURLS;
+    NSUInteger numSamples;
+    AUGraphPlayer *players;
+    BOOL *graphStarted;
+}
+
+-(BOOL)loadAudioURLS:(NSArray*)fileURLArray;
+-(void)loadAUFilePlayers;
+-(void)playSample:(NSUInteger)sampleInArray;
+-(void)stopSample:(NSUInteger)sampleInArray;
+-(void)stopPlayers;
+-(void)stopAndClosePlayers;
+
+@end
